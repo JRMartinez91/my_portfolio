ef7861354eb1d8d02ae26484a544f0449d908e67
James Martinez <airtorque1@gmail.com>
2020-07-21 18:39:22 -0400

    Updates

---
+++ precache-manifest.29994875c1f6146e3c31ecaffec9f740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b49c2b37f6b96f91e3c24373cb62e00",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "524c7ed4941c2ed9afb7",
+    "url": "/my_portfolio/static/css/main.46fb33db.chunk.css"
+  },
+  {
+    "revision": "579dc81937fc6cfd9c49",
+    "url": "/my_portfolio/static/js/2.e011c0c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my_portfolio/static/js/2.e011c0c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "524c7ed4941c2ed9afb7",
+    "url": "/my_portfolio/static/js/main.d9c15e2d.chunk.js"
+  },
+  {
+    "revision": "d9629bd1ee9955b69a66",
+    "url": "/my_portfolio/static/js/runtime-main.88b1f98b.js"
+  }
+]);
